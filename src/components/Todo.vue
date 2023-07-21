2b1f15944b157e68dc2e4ca309da05b73022782f
Md-shefat-masum <mshefat924@gmail.com>
2023-07-22 01:01:07 +0600

    todo component designed

---
+++ src/components/Todo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="todo_app">
+        <div class="card">
+            <div class="todo_app_body">
+                <div class="todo_header">
+                    <span class="material-symbols-outlined fill">checklist</span>
+                    <div>
+                        Todo List
+                    </div>
+                </div>
+                <div class="todo_list custom_scroll">
+                    <div class="item" v-for="i in 10" :key="i">
+                        <div class="check">
+                            <input type="checkbox" class="form_checkbox">
+                        </div>
+                        <div class="description">
+                            <p>Lorem ipsum dolor sit.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="todo_create">
+                    <input placeholder="Add a task" type="text">
+                    <button class="btn">
+                        <span class="material-symbols-outlined fill">send</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
