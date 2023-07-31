4393acbc017a3857a64111d498fdfaea6fd0321f
Md-shefat-masum <mshefat924@gmail.com>
2023-08-01 02:22:12 +0600

    add dashboard middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,8 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'dashboard_auth' => \App\Http\Middleware\DashboardAuth::class,
+        'dashboard_check_auth' => \App\Http\Middleware\DashboardCheckAuth::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
