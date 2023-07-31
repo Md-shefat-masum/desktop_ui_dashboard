c48c7e720b9893f39d77816234a36164d13f0b31
Md-shefat-masum <mshefat924@gmail.com>
2023-08-01 02:24:39 +0600

    add new js

---
+++ resources/js/new.js
@@ -0,0 +1,10 @@
+/*
+    Project: dashboard_laravel_10
+    User: MD Shefat Masum
+    Github: https://github.com/MD Shefat Masum
+    Date: 24 July 2023
+    Time: 18:45:11
+    Path: resources\js\new.js
+*/
+ 
+
